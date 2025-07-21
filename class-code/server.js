166c874393ba6482f20bd633ea7396df60c21647
omarakamal <omaraakamal@gmail.com>
2025-07-21 14:22:50 +0300

    update

--- class-code/server.js
+++ class-code/server.js
@@ -5,11 +5,26 @@ const app = express() // creates a express application
 // Middleware
 app.use(express.static('public')); //all static files are in the public folder
 
+// database
+const students = [
+    {
+        name: "Malak",
+        id: 1
+    },
+    {
+        name: "Muhannad",
+        id: 2
+    },
+    {
+        name: "Bayan",
+        id:3
+    }
+]
 
 
 // Routes go here
 app.get("/",(req,res)=>{
-    res.render("home.ejs")
+    res.render("home.ejs",{language: "JavaScript"})
 })
 
 app.listen(3000,()=>{
